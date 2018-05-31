6fcba4df75ca1dea712179104eed09c1780bc43c
zelalem <zelalem.agmasse@gmail.com>
2018-05-31 15:55:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#commenting a program 
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+/*zelalem a.Agmasse
+  I am 35
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
